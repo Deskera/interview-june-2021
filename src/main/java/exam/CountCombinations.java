54957dd974dadfc43129f04db1584c1b05caebf3
Aditi Jagtap <70377903+7057344018@users.noreply.github.com>
2021-06-22 13:01:01 +0530

    Update CountCombinations.java

--- src/main/java/exam/CountCombinations.java
+++ src/main/java/exam/CountCombinations.java
@@ -3,8 +3,41 @@ package exam;
 public class CountCombinations {
 
   public int getNumberOfWays(final int total, final int k) {
-    //TODO write your code here to achieve the desired result as explained in Readme file for this problem.
-    return 0;
+    int arr[] = {1, 2, 3, 4, 5};   
+    //Calculate length of array arr  
+    int length = sizeof(arr)/sizeof(arr[0]);  
+    //n determine the number of times an array should be rotated  
+    int n = 3;  
+      
+    //Displays original array  
+    printf("Original array: \n");  
+    for (int i = 0; i < length; i++) {   
+        printf("%d ", arr[i]);   
+    }    
+      
+    //Rotate the given array by n times toward left  
+    for(int i = 0; i < n; i++){  
+        int j, first;  
+        //Stores the first element of the array  
+        first = arr[0];  
+      
+        for(j = 0; j < length-1; j++){  
+            //Shift element of array by one  
+            arr[j] = arr[j+1];  
+        }  
+        //First element of array will be added to the end  
+        arr[j] = first;  
+    }  
+      
+    printf("\n");  
+      
+    //Displays resulting array after rotation  
+    printf("Array after left rotation: \n");  
+    for(int i = 0; i < length; i++){  
+        printf("%d ", arr[i]);  
+    }  
+    return 0;  
+    //TODO write your code here to achieve the desired result as explained in Readme file for this problem
   }
 
 }
