1427d54f557a9bc4d8afb2ac1f67743af80330b7
Vaibhavkadam96 <86231570+Vaibhavkadam96@users.noreply.github.com>
2021-06-22 12:56:11 +0530

    Update ShiftElementsInArray.java

--- src/main/java/exam/ShiftElementsInArray.java
+++ src/main/java/exam/ShiftElementsInArray.java
@@ -1,10 +1,46 @@
 package exam;
+import java.util.*;
 
 public class ShiftElementsInArray {
 
   public void shift(int[] arr) {
     //TODO write your code here to achieve the desired result as explained in Readme file for this problem.
+    	Scanner sc = new Scanner(System.in);
+		
+	        int i,n,temp;
+		
+		System.out.println("Enter the number of elements:") ;
+ 		n = sc.nextInt();
+		int[] a = new int[n];
+ 
+    		System.out.println("Enter the elements") ;
+    		for(i=0;i<n;i++)
+    		{ 
+        		a[i] = sc.nextInt();
+    		}
+		
+	    	System.out.println("Original array");
+    		for(i=0;i<n;i++)
+    		{
+        		System.out.print(a[i]+" ");
+    		}
+ 
+    	
+    		temp=a[n-1];
+    		for(i=n-1;i>0;i--)
+    		{
+       	 		a[i]=a[i-1];
+    		}
+    		a[0]=temp;
+ 
+    		System.out.println("\nNew array after rotating by one postion in the right direction");
+    		for(i=0;i<n;i++)
+    		{
+       	 		System.out.print(a[i]+" ");
+    		}
 
   }
 
 }
+
+
