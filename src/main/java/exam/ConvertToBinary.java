efa79b263142e53080c244cb63cab4b5153b262a
Bharti2804 <86234927+Bharti2804@users.noreply.github.com>
2021-06-22 12:46:53 +0530

    this is necessary

--- src/main/java/exam/ConvertToBinary.java
+++ src/main/java/exam/ConvertToBinary.java
@@ -1,9 +1,15 @@
 package exam;
 
 public class ConvertToBinary {
+  static void decimaltoBinary(int num)
 
-  public String toBinary(final int n) {
+  {
+    System.out.println(Integer.toBinaryString(num));
+  }
+    
+ public static void main (String[]) args)
+ { int num = 34;
     //TODO write your code here to achieve the desired result as explained in Readme file for this problem.
-    return "";
+    decimalToBinary(num);
   }
 }
