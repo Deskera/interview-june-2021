ebdd4e860d17c7730d2c2ed7dfb622e54dc6a901
SaurabhAdep <66101239+SaurabhAdep@users.noreply.github.com>
2021-06-22 12:59:05 +0530

    Update ConvertToBinary.java

--- src/main/java/exam/ConvertToBinary.java
+++ src/main/java/exam/ConvertToBinary.java
@@ -3,7 +3,26 @@ package exam;
 public class ConvertToBinary {
 
   public String toBinary(final int n) {
-    //TODO write your code here to achieve the desired result as explained in Readme file for this problem.
+           int remainder;
+
+        if (n <= 1) {
+            System.out.print(n);
+            return; 
+        }
+
+        remainder = n % 2;
+        System.out.print(remainder);
+
+        if (n < 0) {
+            System.out.println("Error: Not a positive integer");
+        } 
+    else {
+
+            System.out.print("Convert to binary is:");
+            toBinary(n);
+        }
+    }
+
     return "";
   }
 }
