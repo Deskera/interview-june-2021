a1b5a09546f1c38ce965390995cfb801aefea532
Akashdeep Tyagi <63424948+Akashdeeptyagi@users.noreply.github.com>
2021-06-22 12:56:42 +0530

    Update CountCombinations.java

--- src/main/java/exam/CountCombinations.java
+++ src/main/java/exam/CountCombinations.java
@@ -1,10 +1,17 @@
-package exam;
-
-public class CountCombinations {
-
-  public int getNumberOfWays(final int total, final int k) {
+public int getNumberOfWays(final int total, final int k) {
     //TODO write your code here to achieve the desired result as explained in Readme file for this problem.
+    int n=1;
+    for(int i=0;i<n;i++)
+    {
+     int j,last;
+      last=arr[arr.length-1];
+      for(j=arr.length-1;j>0;j--)
+      {
+        arr[j]=arr[j-1];
+      }
+      arr[0]=last;
+    }
+    for(int i=0;i<arr.length;i++)
+      System.out.print(arr[i]+" ");
     return 0;
   }
-
-}
