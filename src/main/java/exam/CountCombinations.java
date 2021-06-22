98e9ede99f7c7802ba118166a41d889119ce7a14
harshu1999 <86244822+harshu1999@users.noreply.github.com>
2021-06-22 13:17:41 +0530

    Update CountCombinations.java

--- src/main/java/exam/CountCombinations.java
+++ src/main/java/exam/CountCombinations.java
@@ -2,9 +2,50 @@ package exam;
 
 public class CountCombinations {
 
-  public int getNumberOfWays(final int total, final int k) {
+  public int getNumberOfWays(final int arr[], final int index,
+                                 final int num,final  int reducedNum) {
+    if (reducedNum < 0)
+        return;
+ 
+    if (reducedNum == 0)
+    {
+        for (int i = 0; i < index; i++)
+                System.out.print (arr[i] + " ");
+            System.out.println();
+        return;
+    }
+ 
+    int prev = (index == 0) ?
+                          1 : arr[index - 1];
+ 
+    
+    for (int k = prev; k <= num ; k++)
+    {
+      
+        arr[index] = k;
+ 
+     
+        findCombinationsUtil(arr, index + 1, num,
+                                 reducedNum - k);
+    }
+}
+
+static void findCombinations(int n)
+{
+   
+    int arr[] = new int [n];
+ 
+    
+    findCombinationsUtil(arr, 0, n, n);
+}
+ 
+
+public static void main (String[] args)
+{
+    int n=5;
+    //findCombinations(n);
     //TODO write your code here to achieve the desired result as explained in Readme file for this problem.
-    return 0;
+    return n;
   }
 
 }
