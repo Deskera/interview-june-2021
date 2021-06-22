47f1da81ffc7db307dcab2c3e41ab48aac39cd97
pranjal9798 <86240983+pranjal9798@users.noreply.github.com>
2021-06-22 00:26:42 -0700

    Update CountCombinations.java

--- src/main/java/exam/CountCombinations.java
+++ src/main/java/exam/CountCombinations.java
@@ -1,10 +1,54 @@
-package exam;
+import java.io.*;
+ 
+class GFG
+{
+    
+static void findCombinationsUtil(int arr[], int index,
+                                 int num, int reducedNum)
+{
+    // Base condition
+    if (reducedNum < 0)
+        return;
+ 
+    // If combination is
+    // found, print it
+    if (reducedNum == 0)
+    {
+        for (int i = 0; i < index; i++)
+                System.out.print (arr[i] + " ");
+            System.out.println();
+        return;
+    }
+ 
+   
+    int prev = (index == 0) ?
+                          1 : arr[index - 1];
+ 
+    for (int k = prev; k <= num ; k++)
+    {
+      
+        arr[index] = k;
+ 
+       
+        findCombinationsUtil(arr, index + 1, num,
+                                 reducedNum - k);
+    }
+}
+ 
 
-public class CountCombinations {
-
-  public int getNumberOfWays(final int total, final int k) {
-    //TODO write your code here to achieve the desired result as explained in Readme file for this problem.
-    return 0;
-  }
-
+static void findCombinations(int n)
+{
+   
+    int arr[] = new int [n];
+ 
+    // find all combinations
+    findCombinationsUtil(arr, 0, n, n);
+}
+ 
+// Driver code
+public static void main (String[] args)
+{
+    int n = 5;
+    findCombinations(n);
+}
 }
