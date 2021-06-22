9b1bb8150cbbd639176a1c2333823fe81928ef6c
swetambariandhale <86236984+swetambariandhale@users.noreply.github.com>
2021-06-22 12:38:44 +0530

    Update ShiftElementsInArray.java
    package exam;
    
    public class ShiftElementsInArray {
    
      public void shift(int[] arr) {
    
    
        public static void main(String[] args) {
    
            Scanner input = new Scanner (System.in);
            int array[] = new int [5];
            int array1[] = new int [5];
            int i, temp;
    
            for (i=0; i<5; i++) {
                System.out.printf("Enter array[%d]: \n", i);
                array[i] = input.nextInt(); 
            }
    
            System.out.println("\nEntered datas are: \n");
            for (i=0; i<5; i++) {
                System.out.printf("array[%d] = %d\n", i, array[i]); 
            }
    
            temp = array[4];
            System.out.println("\nAfter Shifting: \n");
    
            for(i=3; i>=0; i--) {
                array1[i+1] = array[i]; 
                array1[0] = temp; 
            }
    
    
            for (i=0; i<5; i++) {
                System.out.printf("array[%d] = %d\n", i, array1[i]);
            }
    
            input.close();
    
    
    }
      }
    
    }

--- src/main/java/exam/ShiftElementsInArray.java
+++ src/main/java/exam/ShiftElementsInArray.java
@@ -3,8 +3,42 @@ package exam;
 public class ShiftElementsInArray {
 
   public void shift(int[] arr) {
-    //TODO write your code here to achieve the desired result as explained in Readme file for this problem.
 
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner (System.in);
+        int array[] = new int [5];
+        int array1[] = new int [5];
+        int i, temp;
+
+        for (i=0; i<5; i++) {
+            System.out.printf("Enter array[%d]: \n", i);
+            array[i] = input.nextInt(); 
+        }
+
+        System.out.println("\nEntered datas are: \n");
+        for (i=0; i<5; i++) {
+            System.out.printf("array[%d] = %d\n", i, array[i]); 
+        }
+
+        temp = array[4];
+        System.out.println("\nAfter Shifting: \n");
+
+        for(i=3; i>=0; i--) {
+            array1[i+1] = array[i]; 
+            array1[0] = temp; 
+        }
+
+
+        for (i=0; i<5; i++) {
+            System.out.printf("array[%d] = %d\n", i, array1[i]);
+        }
+
+        input.close();
+
+
+}
   }
 
 }
