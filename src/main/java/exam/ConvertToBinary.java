bef2f0cb4a7ca800ec727b705836753d63241dea
PallaviMandekar <86230846+PallaviMandekar@users.noreply.github.com>
2021-06-22 09:58:31 +0300

    integer.java

--- src/main/java/exam/ConvertToBinary.java
+++ src/main/java/exam/ConvertToBinary.java
@@ -3,7 +3,23 @@ package exam;
 public class ConvertToBinary {
 
   public String toBinary(final int n) {
-    //TODO write your code here to achieve the desired result as explained in Readme file for this problem.
+    //TODO write your code here to achieve the desired result as explained in Readme file for this problem.public class ListDirectoryRecusive {
+   public static void main(String[] args) {
+      File dir = new File("d:\\myproject\\test");  // Escape sequence needed for '\'
+      listRecursive(dir);
+   }
+   
+   public static void listRecursive(File dir) {
+      if (dir.isDirectory()) {
+         File[] items = dir.listFiles();
+         for (File item : items) {
+            System.out.println(item.getAbsoluteFile());
+            if (item.isDirectory()) listRecursive(item);  // Recursive call
+         }
+      }
+   }
+}
+    
     return "";
   }
 }
